6f4d4716eef42c491e941c8bfec367019f1d2f69
star2112 <bintangfadel73@gmail.com>
2022-10-23 10:21:49 +0700

    localstorage and logout

---
+++ checklogin.js
@@ -0,0 +1,14 @@
+let navlog = document.getElementById("navlog");
+
+function clearLocal() {
+  localStorage.clear();
+  location.href = "signin.html";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("user") == "anisa@gmail.com") {
+    navlog.innerHTML = `<button class="btn btn-sm btn-outline-success me-3" onclick="clearLocal()">Logout</button>`;
+  } else {
+    navlog.innerHTML = `<button class="btn btn-sm btn-outline-success me-3" onclick="location.href='signup.html'">Sign Up</button><button class="btn btn-sm btn-success" onclick="location.href='signin.html'">Sign In</button>`;
+  }
+});
--- login.js
+++ login.js
@@ -1,39 +1,27 @@
+let submitClicked = document.querySelector("#submit");
 
-    let submitClicked = document.querySelector("#submit");
+fetch("https://634fe40678563c1d82b2b716.mockapi.io/be4/user")
+  .then((result) => result.json())
+  .then((result) => {
+    console.log((user = result));
+  });
 
-    fetch("https://634fe40678563c1d82b2b716.mockapi.io/be4/user")
-                    .then(result => result.json())
-                    .then(result => {console.log(user = result)})
+let user = [];
 
-    let user = [];
+submitClicked.addEventListener("click", function (event) {
+  event.preventDefault();
+  let emailInput = document.querySelector("#email").value;
+  let passwordInput = document.querySelector("#password").value;
 
-    submitClicked.addEventListener("click", function(event){
-        event.preventDefault()
-        let emailInput = document.querySelector("#email").value;
-        let passwordInput = document.querySelector("#password").value;
-
-        for (i=0; i < user.length; i++) {
-            if (emailInput == user[i].email && passwordInput == user[i].password ){
-                console.log(emailInput +" "+ "berhasil login")
-                // alert("anda berhasil login")
-                window.location.href = "berandaUser.html";
-                console.log("tes")
-                return
-        }
+  for (i = 0; i < user.length; i++) {
+    if (emailInput == user[i].email && passwordInput == user[i].password) {
+      console.log(emailInput + " " + "berhasil login");
+      // alert("anda berhasil login")
+      window.location.href = "index.html";
+      localStorage.setItem("user", user[i].email);
+      return;
     }
-        console.log("email atau password salah")
-        alert("email atau password tidak terdaftar!");
-    });
-
-
-
-    
-
-        
-
-
-
-
-
-
-
+  }
+  console.log("email atau password salah");
+  alert("email atau password tidak terdaftar!");
+});
