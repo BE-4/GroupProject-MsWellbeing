e275f0b3ffb5b8769ec042191acd457396bfc9f0
star2112 <bintangfadel73@gmail.com>
2022-10-24 10:34:36 +0700

    fix login

--- checklogin.js
+++ checklogin.js
@@ -5,8 +5,10 @@ function clearLocal() {
   location.href = "signin.html";
 }
 
+console.log(localStorage.getItem("user"));
+
 document.addEventListener("DOMContentLoaded", () => {
-  if (localStorage.getItem("user") !== undefined) {
+  if (localStorage.getItem("user") !== null) {
     navlog.innerHTML = `<button class="btn btn-sm btn-outline-success me-3" onclick="clearLocal()">Logout</button>`;
   } else {
     navlog.innerHTML = `<button class="btn btn-sm btn-outline-success me-3" onclick="location.href='signup.html'">Sign Up</button><button class="btn btn-sm btn-success" onclick="location.href='signin.html'">Sign In</button>`;
